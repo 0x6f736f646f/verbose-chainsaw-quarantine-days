5c9a0818e9bc7f5f1a23964368a6fa0f3e5832b6
0x6f736f646f <blackd0t@protonmail.com>
2020-04-03 13:12:31 +0300

    Adding hyper microservice rest api

---
+++ RustJourney/microservices/a02_hyper_microservice_rust/src/main.rs
@@ -0,0 +1,105 @@
+use std::fmt;
+use std::sync::{Arc, Mutex};
+use slab::Slab;
+use futures::{future, Future};
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use hyper::service::service_fn;
+
+const INDEX: &str = r#"
+<!doctype html>
+<html>
+    <head>
+        <title>Rust Microservice</title>
+    </head>
+    <body>
+        <h3>Rust Microservice</h3>
+    </body>
+</html>
+"#;
+
+type UserId = u64;
+struct UserData;
+impl fmt::Display for UserData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("{}")
+    }
+}
+
+type UserDb = Arc<Mutex<Slab<UserData>>>;
+
+const USER_PATH: &str = "/user/";
+
+fn microservice_handler(req: Request<Body>, user_db: &UserDb) -> impl Future<Item=Response<Body>, Error=Error> {
+    let response = {
+        match (req.method(), req.uri().path()) {
+            (&Method::GET, "/") => {
+                Response::new(INDEX.into())
+            },
+            (method, path) if path.starts_with(USER_PATH) => {
+                let user_id = path.trim_start_matches(USER_PATH)
+                    .parse::<UserId>()
+                    .ok()
+                    .map(|x| x as usize);
+                let mut user = user_db.lock().unwrap();
+                match (method, user_id) {
+                    (&Method::GET, Some(id)) => {
+                        if let Some(data) = user.get(id){
+                            Response::new(data.to_string().into())
+                        } else {
+                            response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    (&Method::POST, None) => {
+                        let id = user.insert(UserData);
+                        Response::new(id.to_string().into())
+                    },
+                    (&Method::POST, Some(_)) => {
+                        response_with_code(StatusCode::BAD_REQUEST)
+                    },
+                    (&Method::PUT, Some(id)) => {
+                        if let Some(user) = user.get_mut(id){
+                            *user = UserData;
+                            response_with_code(StatusCode::OK)
+                        } else {
+                             response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    (&Method::DELETE, Some(id)) => {
+                        if user.contains(id) {
+                            user.remove(id);
+                             response_with_code(StatusCode::OK)
+                        } else {
+                             response_with_code(StatusCode::NOT_FOUND)
+                        }
+                    },
+                    _ => {
+                         response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+                    },
+                }
+            },
+            _ => {
+                 response_with_code(StatusCode::NOT_FOUND)
+            },
+        }
+    };
+    future::ok(response)
+}
+
+fn response_with_code(status_code: StatusCode) -> Response<Body> {
+    Response::builder()
+        .status(status_code)
+        .body(Body::empty())
+        .unwrap()
+}
+
+fn main() {
+    let addr = ([127, 0, 0, 1], 8000).into();
+    let builder = Server::bind(&addr);
+    let user_db = Arc::new(Mutex::new(Slab::new()));
+    let server = builder.serve(move || {
+        let user_db = user_db.clone();
+        service_fn(move |req| microservice_handler(req, &user_db))
+    });
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
+}
